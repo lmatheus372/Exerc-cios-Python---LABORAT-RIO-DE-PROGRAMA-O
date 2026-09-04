1566ba3f933d34c169b37178a67c5a06b2877309
lmatheus372 <lucasreis372@gmail.com>
2026-09-04 13:50:49 -0300

    Atualizando atividades de Python

--- Atividade 16.py
+++ Atividade 16.py
@@ -1,1 +1,2 @@
-aaaa
+for i in range(10, 0, -1):
+    print(i)
---
+++ Atividade 17.py
@@ -0,0 +1,7 @@
+total_arrecadado = 0
+
+for arrecadou in range(10):
+    arrecadou = int(input("Digite o valor arrecadado: "))
+    total_arrecadado = total_arrecadado + arrecadou
+
+print(f"O total arrecadado foi: {total_arrecadado}")
